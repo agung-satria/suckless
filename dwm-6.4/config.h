0c39d3a3babd62068027d4086859677f61d3eeb6
Agung Satria <123456789+agungsatria890@gmail.com>
2023-01-29 00:23:15 +0700

    add systray and toggle to dwm64 OK

--- dwm-6.4/config.h
+++ dwm-6.4/config.h
@@ -12,6 +12,11 @@ static const unsigned int gappiv    = 14;       /* vert inner gap between window
 static const unsigned int gappoh    = 18;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 28;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static       int showsystray        = 0;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]        = {"SF Pro Display:weight=Medium:size=14:antialias=true:autohint=true",
@@ -66,6 +71,8 @@ static const Rule rules[] = {
   { "Gimp",	    NULL,			  NULL,		  1 << 5,	    	   0,         0,        0,            0,		   -1 },
   { "Yad",	    NULL,			  NULL,		  0,			         0,        -1,        0,            1,		   -1 },
   { "Arandr",	  NULL,			  NULL,		  0,			         0,        -1,        0,            1,		   -1 },
+  { "xdman-Main", NULL,		  NULL,		  0,			         0,        -1,        0,            1,		   -1 },
+  { "SimpleScreenRecorder", NULL,NULL,0,			         0,        -1,        0,            1,		   -1 },
   { "Galculator",NULL,			NULL,		  0,			         0,        -1,        0,            1,		   -1 },
   { "Dragon-drop",NULL,		  NULL,		  0,			         0,        -1,        0,            1,		   -1 },
   { "float-st", NULL,       NULL,     0,               1,         1,        1,            0,       -1 },
@@ -129,6 +136,7 @@ static const Key keys[] = {
   { MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
   { MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
   { MODKEY,                       XK_b,      togglebar,      {0} },
+  { MODKEY,                       XK_g,      togglesystray,  {0} },
   { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
   { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
   { MODKEY,			XK_i,	shiftview,	{ .i = -1 } },
@@ -206,6 +214,7 @@ static const Key keys[] = {
   { MODKEY,				    XK_grave,        spawn,		SHCMD("dmenuunicode") },
   { MODKEY,				    XK_F3,        spawn,		SHCMD("dmenuhandler") },
   { MODKEY,				    XK_v,        spawn,		SHCMD("dmenurecord") },
+  { MODKEY,				    XK_p,        spawn,		SHCMD("arandr") },
   { MODKEY,			      XK_F4,		      spawn,		SHCMD(TERMINAL " -c float-st -g 95x28 -e pulsemixer") },
   { MODKEY|ShiftMask,	XK_r,        spawn,	    SHCMD(TERMINAL " -c float-st -g 95x28 -e gotop") },
   { MODKEY,			      XK_F11,		spawn,		SHCMD("mpv --untimed --no-cache --no-osc --no-input-default-bindings --profile=low-latency --input-conf=/dev/null --title=webcam $(ls /dev/video[0,2,4,6,8] | tail -n 1)") },
@@ -253,6 +262,8 @@ static const Button buttons[] = {
   { ClkTagBar,            0,              Button1,        view,           {0} },
   { ClkTagBar,            0,              Button3,        toggleview,     {0} },
   { ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
   { ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
 
